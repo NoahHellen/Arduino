e1e374c4be16d3de24ae4e9a806f3543894ec936
NoahHellen <noahhellen@gmail.com>
2024-09-07 14:41:23 +0100

    Added all current projects

---
+++ Knock_lock/Knock_lock.ino
@@ -0,0 +1,86 @@
+#include <Servo.h> // Port in Servo library
+Servo myServo; // Create Servo object
+
+// Set variables for INPUT and OUTPUT pins
+const int piezo = A0;
+const int switchPin = 2;
+const int yellowLed = 3;
+const int greenLed = 4;
+const int redLed = 5;
+
+// Set variables for piezo and switch
+int knockVal;
+int switchVal;
+
+// Set variables for piezo sound
+const int quietKnock = 10;
+const int loudKnock = 100;
+
+// Set more variables for piezo and servo
+boolean locked = false;
+int numberOfKnocks = 0;
+
+// Set up OUTPUT and INPUTS of pins
+void setup() {
+  myServo.attach(9);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(switchPin, INPUT);
+  Serial.begin(9600); // Serial object sets up communication between computer and Arduino
+  digitalWrite(greenLed, HIGH); // Initialise green LED to be on from setup
+  myServo.write(0); // Sets angle of Servo to 0 degrees
+  Serial.println("The box is unlocked!"); // Prints message on computer
+}
+
+  // The switch is how we lock the box
+void loop() {
+  if(locked == false){
+    switchVal = digitalRead(switchPin);
+    if(switchVal == HIGH){
+      locked = true;
+      digitalWrite(greenLed, LOW);
+      digitalWrite(redLed, HIGH);
+      myServo.write(90);
+      Serial.println("The box is locked!");
+      delay(1000);
+    }
+  }
+  // The piezo is how we unlock the box
+  if(locked == true){
+    knockVal = analogRead(piezo);
+    if(numberOfKnocks < 5 && knockVal > 0){
+      if(checkForKnock(knockVal) == true){
+      numberOfKnocks++;
+     }   
+    Serial.print(5-numberOfKnocks);
+    Serial.println(" more knocks to go");
+    }
+    if(numberOfKnocks > 4){
+      locked = false;
+      myServo.write(0);
+      delay(20);
+      digitalWrite(greenLed, HIGH);
+      digitalWrite(redLed, LOW);
+      Serial.println("The box is unlocked");
+      numberOfKnocks = 0;
+    }
+  }
+}
+
+// Create function to check for valid knock value
+boolean checkForKnock(int value){
+  if(value > quietKnock && value < loudKnock){
+    digitalWrite(yellowLed, HIGH);
+    delay(50);
+    digitalWrite(yellowLed, LOW);
+    Serial.print("Valid knock of value ");
+    Serial.println(value);
+    return true;
+  }
+  else{
+    Serial.print("Bad knock value ");
+    Serial.println(value);
+    return false;
+  }
+}
---
+++ Zoetrope/Zoetrope.ino
@@ -0,0 +1,75 @@
+const int controlPin1 = 2; //connected to pin 7 on the H-bridge
+const int controlPin2 = 3; //connected to pin 2 on the H-bridge
+const int enablePin = 9; // connected to pin 1 on the H-bridge
+const int directionSwitchPin = 4; // connected to the switch for directoin
+const int onOffSwitchStateSwitchPin = 5; // connected to the switch for motor on/off
+const int potPin = A0; // connected to the potentiometer's output
+
+// create some variables to hold values from inputs
+int onOffSwitchState = 0; // current state of the on/off swtich
+int previousOnOffSwitchState = 0; // previous state of the on/off switch
+int directionSwitchState = 0; // current state of direction switch
+int previousDirectionSwitchState = 0; // previous state of direction switch
+
+int motorEnabled = 0; // controls on/off state of motor
+int motorSpeed = 0; // controls speed of motor
+int motorDirection = 1; // controls direction of motor
+
+void setup() {
+  pinMode(directionSwitchPin, INPUT);
+  pinMode(onOffSwitchStateSwitchPin, INPUT);
+  pinMode(controlPin1, OUTPUT);
+  pinMode(controlPin2, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin, LOW);
+
+}
+
+void loop() {
+  // read the value of the on/off switch
+  onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
+  delay(1);
+
+  // read the value of the direction swtich
+  directionSwitchState = digitalRead(directionSwitchPin);
+
+  // set the speed of the motor based on the potentiometer
+  motorSpeed = analogRead(potPin)/4;
+
+  // if/then statement for pressing on/off button
+  if(onOffSwitchState != previousOnOffSwitchState){
+    if(onOffSwitchState == HIGH){
+      motorEnabled = !motorEnabled;
+    }
+  }
+
+  // if/then statement for pressing direction button
+  if(directionSwitchState != previousDirectionSwitchState){
+    if(directionSwitchState == HIGH) {
+      motorDirection = !motorDirection;
+    }
+  }
+
+  // code for outputting if/then statements into circuit using digitalWrite
+  if(motorDirection == 1){
+    digitalWrite(controlPin1, HIGH);
+    digitalWrite(controlPin2, LOW);
+  }
+  else {
+    digitalWrite(controlPin1, LOW);
+    digitalWrite(controlPin2, HIGH);
+
+  // code for outputting if/then statements into circuit using analogWrite
+  }
+  if(motorEnabled == 1){
+    analogWrite(enablePin, motorSpeed);
+  }
+  else {
+    analogWrite(enablePin, 0);
+  }
+  previousDirectionSwitchState = directionSwitchState;
+  previousOnOffSwitchState = onOffSwitchState;
+
+}
+
+
